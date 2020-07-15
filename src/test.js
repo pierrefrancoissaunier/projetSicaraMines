1f92f4ca4206bf679fe3a86f98430acc2cb9b868
pierrefrancoissaunier <56261508+pierrefrancoissaunier@users.noreply.github.com>
2020-07-15 09:27:07 +0200

    Delete test.js

--- src/test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export default ({style, state}) => (
-        <Text style={style}>
-            {state.texte}
-        </Text>
-);
